3a26b1cff31bd1a5c8509ff4dbbb6f980f8ea1e4
Cassius Souza <cassius.sanches@gmail.com>
2020-12-07 14:10:01 -0300

    Copying challenge 29

---
+++ dom.js
@@ -0,0 +1,92 @@
+(function (win, document) {
+	'use strict';
+
+	function DOM(elements) {
+		if (!(this instanceof DOM)) {
+			return new DOM(elements);
+		}
+		this.elements = document.querySelectorAll(elements);
+	}
+
+	DOM.isArray = function isArray(param) {
+		return Object.prototype.toString.call(param) === '[object Array]';
+	};
+
+	DOM.isFunction = function isFunction(param) {
+		return Object.prototype.toString.call(param) === '[object Function]';
+	};
+
+	DOM.isNumber = function isNumber(param) {
+		return Object.prototype.toString.call(param) === '[object Number]';
+	};
+
+	DOM.isString = function isString(param) {
+		return Object.prototype.toString.call(param) === '[object String]';
+	};
+
+	DOM.isBoolean = function isBoolean(param) {
+		return Object.prototype.toString.call(param) === '[object Boolean]';
+	};
+
+	DOM.isNull = function isNull(param) {
+		return (
+			Object.prototype.toString.call(param) === '[object Null]' ||
+			Object.prototype.toString.call(param) === '[object Undefined]'
+		);
+	};
+
+	// DOM.prototype.getDOMElements = function getDOMElements(elements) {
+	//   return document.querySelectorAll(elements);
+	// };
+	DOM.prototype.on = function on(eventType, callback) {
+		Array.prototype.forEach.call(this.elements, function (element) {
+			element.addEventListener(eventType, callback, false);
+		});
+	};
+
+	DOM.prototype.off = function off(eventType, callback) {
+		Array.prototype.forEach.call(this.elements, function (element) {
+			element.revemoEventListener(eventType, callback, false);
+		});
+	};
+
+	DOM.prototype.get = function get(index) {
+		if (!index) {
+			return this.elements[0];
+		}
+		return this.elements[index];
+	};
+
+	// Crie os seguintes métodos:
+	// - forEach, map, filter, reduce, reduceRight, every e some.
+
+	DOM.prototype.forEach = function forEach() {
+		return Array.prototype.forEach.apply(this.elements, arguments);
+	};
+
+	DOM.prototype.map = function map() {
+		return Array.prototype.map.apply(this.elements, arguments);
+	};
+
+	DOM.prototype.filter = function filter() {
+		return Array.prototype.filter.apply(this.elements, arguments);
+	};
+
+	DOM.prototype.reduce = function reduce() {
+		return Array.prototype.reduce.apply(this.elements, arguments);
+	};
+
+	DOM.prototype.reduceRight = function reduceRight() {
+		return Array.prototype.reduceRight.apply(this.elements, arguments);
+	};
+
+	DOM.prototype.every = function every() {
+		return Array.prototype.every.apply(this.elements, arguments);
+	};
+
+	DOM.prototype.some = function some() {
+		return Array.prototype.some.apply(this.elements, arguments);
+	};
+
+	win.DOM = DOM;
+})(window, document);
